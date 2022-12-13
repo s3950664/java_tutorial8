ae50a0a10ef6447b001d5b4c7e3927161403bcb8
s3950664 <s3950664@rmit.edu.vn>
2022-12-13 17:58:42 +0700

    first

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.io.FileWriter;
-import java.io.FilenameFilter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -20,10 +19,34 @@ public class Main {
         address = scanner.nextLine();
 
         System.out.println("age: ");
-        age = scanner.nextInt();
+        age = Integer.parseInt(scanner.nextLine());
 
-        pw.println(name+" "+address+" "+age);
+        pw.println(name+","+address+","+age);
         pw.flush();
         pw.close();
+
+        //Ex 2
+
+        Scanner fileScanner = new Scanner(new File("src/users.txt"));
+        String line;
+        double sum = 0;
+        int count = 0;
+
+        while(fileScanner.hasNext()) {
+            count++;
+            line = fileScanner.nextLine();
+
+            StringTokenizer reader = new StringTokenizer(line, ",");
+            if (reader.countTokens() != 3) {
+                throw new IOException("Invalid input format!");
+            } else {
+                name = reader.nextToken();
+                address = reader.nextToken();
+                age = Integer.parseInt(reader.nextToken());
+                sum += age;
+            }
+            System.out.println(name+" lives in "+address+" and is "+age+" years old");
+            System.out.println((float)sum/count);
+        }
     }
 }
